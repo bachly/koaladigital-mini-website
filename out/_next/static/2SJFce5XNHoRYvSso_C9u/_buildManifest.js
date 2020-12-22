d7dbcf273c4e9e7c381fd9fa319b73af99aa4f88
Bach <bly@cochlear.com>
2020-12-22 15:32:29 +1100

    New export

---
+++ out/_next/static/2SJFce5XNHoRYvSso_C9u/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/index":[a],"/project/[slug]":[a]}}("static\u002Fchunks\u002Fec72a9eaf5ac668f37daba989b071ff184458322.9fe5dcc3eaa3a3206200.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
